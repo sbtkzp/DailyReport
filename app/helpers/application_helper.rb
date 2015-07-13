20ded2ded5dc9e1b6c017246c6f9dd5d7bfeb8b6
Shibata Kazuki <d0_n0t_0ften_use_this@yahoo.co.jp>
2015-07-13 23:37:03 +0900

    Markdown.newの引数記述形式変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,7 @@ module ApplicationHelper
   def markdown(text)
     unless @markdown
       renderer = Redcarpet::Render::HTML.new filter_html: true
-      @markdown = Redcarpet::Markdown.new renderer,
-      tables: true
+      @markdown = Redcarpet::Markdown.new(renderer, tables: true)
     end
     @markdown.render(text).html_safe
   end
