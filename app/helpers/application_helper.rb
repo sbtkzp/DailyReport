44fece7e11fac934df5fe4a0c09c2d00c2b1f02b
Shibata Kazuki <d0_n0t_0ften_use_this@yahoo.co.jp>
2015-07-13 18:37:15 +0900

    MDにおける表記法追加とXSS対策

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def markdown(text)
     unless @markdown
-      renderer = Redcarpet::Render::HTML.new
-      @markdown = Redcarpet::Markdown.new renderer
+      renderer = Redcarpet::Render::HTML.new filter_html: true
+      @markdown = Redcarpet::Markdown.new renderer,
+      tables: true
     end
     @markdown.render(text).html_safe
   end
